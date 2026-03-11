c0af9613da15cd74a8a18701bbbbf4af0915e0b9
Dhanu6759 <td3054@srmist.edu.in>
2026-03-11 13:20:02 +0530

    UC4: Render OOPS as Banner RENDER OOPS as banner using string.array

--- printoops.java
+++ printoops.java
@@ -17,7 +17,7 @@
 // effect for each letter in the message. Use print statements for each line
 // instead of a loop.
 
-public class UC3_OOPSBannerApp {
+public class UC4_OOPSBannerApp {
 
     // Main method to run the banner display
     // Print each line of the OOPS banner to create the visual effect for the
@@ -25,27 +25,38 @@ public class UC3_OOPSBannerApp {
     // Each line corresponds to a row in the banner for the letters O, O, P, S.
     // Using individual print statements for each line.
     public static void main(String[] args) {
-	// Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
+	// Hint:
+// 1. Define a String array with a size equal to the number of lines in the banner
+// 2. Populate each index of the array with the corresponding banner line using String.join()
+// 3. Use a for-each loop to iterate through the array and print each line
+// Example of OS banner line creation: Firstly populate the array as below
 
-        System.out.println(String.join(" ",
-                " ** ", " ** ", " ** ", " *** "));
 
-        System.out.println(String.join(" ",
-                "*     ", "     ", "     ", "      "));
+        String[] banner = new String[7];
 
-        System.out.println(String.join(" ",
-                "*     ", "     ", "     ", "      "));
+        banner[0] = String.join(" ",
+                " * ", " * ", " * ", " ** ");
 
-        System.out.println(String.join(" ",
-                "*     ", "     ", "** ", " ** "));
+        banner[1] = String.join(" ",
+                "*     ", "     ", "     ", "      ");
 
-        System.out.println(String.join(" ",
-                "*     ", "     ", "      ", "      *"));
+        banner[2] = String.join(" ",
+                "*     ", "     ", "     ", "      ");
 
-        System.out.println(String.join(" ",
-                "*     ", "     ", "      ", "      *"));
+        banner[3] = String.join(" ",
+                "*     ", "     ", "* ", " * ");
 
-        System.out.println(String.join(" ",
-                " ** ", " ** ", "*      ", " *** "));
+        banner[4] = String.join(" ",
+                "*     ", "     ", "      ", "      *");
+
+        banner[5] = String.join(" ",
+                "*     ", "     ", "      ", "      *");
+
+        banner[6] = String.join(" ",
+                " * ", " * ", "*      ", " * ");
+
+        for (String line : banner) {
+            System.out.println(line);
+			//uc4
+        }
     }
-}
